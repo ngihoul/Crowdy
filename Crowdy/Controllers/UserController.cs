f5315e8b6c5f6e07b1c553eb505e73801371ad37
Nicolas Gihoul <ngihoul@hotmail.com>
2024-10-14 11:25:02 +0200

    Adding Controller for user profile

---
+++ Crowdy/Controllers/UserController.cs
@@ -0,0 +1,25 @@
+﻿using Crowdy.BLL.Services;
+using Crowdy.DAL.Entities;
+using Crowdy.Tools;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Crowdy.Controllers
+{
+    [ConnectedOnly]
+    public class UserController : Controller
+    {
+        private readonly AuthService _authService;
+
+        public UserController(AuthService authService)
+        {
+            _authService = authService;
+        }
+
+        public IActionResult Index()
+        {
+            User user = _authService.GetUser()!;
+
+            return View(user);
+        }
+    }
+}
